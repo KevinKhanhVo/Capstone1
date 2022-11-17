273666d27b2afb785d6e7d9ac5250fb9642bfd53
Kevin <k.vo.vcstech@gmail.com>
2022-11-17 15:14:04 -0600

    Updated variable

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, redirect, flash, session, g
 from models import db, connect_db, Trainer, Pokemon, Move, PokemonType, Location
 from forms import LoginForm, TrainerAddForm
 from werkzeug.utils import secure_filename
-from psycopg2 import connect
 
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt()
@@ -10,17 +9,10 @@ bcrypt = Bcrypt()
 import requests
 import random
 
-conn = connect(
-    host="dpg-cdr9l7da4991vat3bf80-a",
-    database="cichorium",
-    user="kevlovesrice",
-    password="qY3aiPkE9wAGHiXI7c43qvreNz8Rzyey",
-    port=5432
-)
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secretpokemon@app"
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://kevlovesrice:qY3aiPkE9wAGHiXI7c43qvreNz8Rzyey@dpg-cdr9l7da4991vat3bf80-a/cichorium"
 app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
 app.config['DEBUG'] = False
 
