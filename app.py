310692dab1c306d1ee3c8ff73c5e5999201d5971
Kevin <k.vo.vcstech@gmail.com>
2022-11-17 17:36:38 -0600

    Fixing the internal database url

--- app.py
+++ app.py
@@ -2,26 +2,22 @@ from flask import Flask, render_template, redirect, flash, session, g
 from models import db, connect_db, Trainer, Pokemon, Move, PokemonType, Location
 from forms import LoginForm, TrainerAddForm
 from werkzeug.utils import secure_filename
-from psycopg2 import connect
 
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt()
 
 import requests
 import random
+import os
 
-conn = connect(
-    host="dpg-cdr9l7da4991vat3bf80-a",
-    database="cichorium",
-    user="kevlovesrice",
-    password="qY3aiPkE9wAGHiXI7c43qvreNz8Rzyey",
-    port=5432
-)
+uri = os.environ.get("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "secretpokemon@app"
+app.config['SECRET_KEY'] = "SeCrEtOF@llSecretCichorium@pp!"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://kevlovesrice:qY3aiPkE9wAGHiXI7c43qvreNz8Rzyey@dpg-cdr9l7da4991vat3bf80-a/cichorium"
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
 app.config['DEBUG'] = False
 
