bf1652a4682768e9dfdfbe1c91c4dec93d52627a
Kevin <k.vo.vcstech@gmail.com>
2022-11-17 18:15:12 -0600

    Commented out the psycopg2 connect variable

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, flash, session, g
 from models import db, connect_db, Trainer, Pokemon, Move, PokemonType, Location
 from forms import LoginForm, TrainerAddForm
 from werkzeug.utils import secure_filename
-from psycopg2 import connect
+# from psycopg2 import connect
 
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt()
@@ -11,13 +11,13 @@ import requests
 import random
 import os
 
-conn = connect(
-    host="dpg-cdr9l7da4991vat3bf80-a",
-    database="cichorium",
-    user="kevlovesrice",
-    password="qY3aiPkE9wAGHiXI7c43qvreNz8Rzyey",
-    port=5432
-)
+# conn = connect(
+#     host="dpg-cdrc64ta499bn1521sr0-a",
+#     database="cichorium_990g",
+#     user="kevlovesrice",
+#     password="I4a6fzQhHlrhG1itlJcIdjRHp3olvrQM",
+#     port=5432
+# )
 
 uri = os.environ.get("DATABASE_URL")
 if uri.startswith("postgres://"):
